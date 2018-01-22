d49a3184ab4bcf1b4a6fd7cd04ab1e640b9ecef2
Hannah Lee and Kayla Ondracek <kayla@>
2018-01-22 09:32:11 -0800

    add Place constructor to access information about places user has been and also add form

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function Place(location, landmarks, timeOfYear, notes) {
+  this.locationText = location;
+  this.landmarkArray = landmarks;
+  this.timeOfYearText = timeOfYear;
+  this.notesText = notes;
+};
+
+$(document).ready(function() {
+  $("#places").submit(function(event) {
+    event.preventDefault();
+
+    
+  });
+
+});
