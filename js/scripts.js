cf5f87c703e7104b6405a0286c4eca39d5e8854d
Hannah Lee and Kayla Ondracek <kayla@>
2018-01-22 10:17:22 -0800

    add ability to click list of inputted places and display more info about each

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,29 @@ $(document).ready(function() {
   $("#places").submit(function(event) {
     event.preventDefault();
 
-    
+    var location = $("input#location").val();
+    var landmarks = [$("input#landmark1").val(), $("input#landmark2").val(), $("input#landmark3").val()];
+    var timeOfYear = $("input#timeOfYear").val();
+    var notes = $("input#notes").val();
+
+    var newPlace = new Place(location, landmarks, timeOfYear, notes);
+
+    $("ul").append("<li><span class='click'>" + location + "</span></li>")
+
+    $("input#location").val("");
+    $("input#landmark1").val("");
+    $("input#landmark2").val("");
+    $("input#landmark3").val("");
+
+    $(".click").last().click(function() {
+      $(".output-group").show();
+      $(".output-group h2").text(newPlace.locationText);
+      $(".location-output").text(newPlace.locationText);
+      $(".landmarks-output").text(newPlace.landmarkArray.join(", "));
+    });
+
+
+
   });
 
 });
